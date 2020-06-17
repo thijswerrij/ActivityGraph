1fde9b2a248664aeaefc092bfcdcc9efa07c8a4c
Thijs Werrij <thijswerrij@gmail.com>
2020-06-17 18:11:16 +0200

    Basic create, set and delete functionality, activitypub responds to changes in the neo4j graph. TODO: neo4j graph is finalized after activitypub is updated

--- check-db.py
+++ check-db.py
@@ -6,10 +6,24 @@
 from neo4j import GraphDatabase, basic_auth
 from time import time
 
+from activitypub.manager import Manager
+from activitypub.database import *
+
 #%%
 
 driver = GraphDatabase.driver("bolt://localhost", auth=basic_auth("neo4j", "hunter2"))
 session = driver.session()
+
+db = MongoDatabase("mongodb://localhost:27017", "dsblank_localhost")
+manager = Manager(database=db)
+
+if manager.database.table_exists("activities"):
+    manager.database.activities.clear()
+else:
+    manager.database.build_table("activities")
+manager.database.activities.clear()
+manager.database.actors.clear()
+manager.database.objects.clear()
 
 #%% Functions
 
@@ -38,35 +52,76 @@ for record in results:
     
 #%%
 
-match_new_nodes = """
+match_nodes = """
 MATCH (n)
-WHERE n.graph_status = "new"
+WHERE n.graph_status = "%s"
 RETURN n, labels(n), keys(n)
 """
 
-match_new_edges = """
+match_edges = """
 MATCH (a)-[e]->(b)
-WHERE e.graph_status = "new"
-RETURN e
+WHERE e.graph_status = "%s"
+RETURN e, type(e)
 """
 
 def checkForUpdates():
-    node_results = session.run(match_new_nodes)
-    edge_results = session.run(match_new_edges)
+    created_nodes = session.run(match_nodes % "new")
+    created_edges = session.run(match_edges % "new")
     
-    edges = []
+    updated_nodes = session.run(match_nodes % "updated")
+    updated_edges = session.run(match_edges % "updated")
     
-    if edge_results:
-        print('New edges')
-        for e in edge_results:
-            edges.append(e[0])
+    deleted_nodes = session.run(match_nodes % "delete")
+    deleted_edges = session.run(match_edges % "delete")
     
-    if node_results:
+    detached_nodes = session.run(match_nodes % "detach")
+    
+    if created_nodes.peek():
         print('New nodes')
-        for record in node_results:
-            createObject(record[0], record[1], record[2], edges)
+        for record in created_nodes:
+            createObject(record[0], record[1], record[2])
+            
+    if created_edges.peek():
+        print('New edges')
+        for e in created_edges:
+            nodes = e[0].nodes
+            print(e)
+            createEdge(nodes[0],nodes[1],e[1])
+    
+    if updated_edges.peek():
+        print('Updated edges')
+        for e in updated_edges:
+            #TODO?
+            print(e)
+    
+    if updated_nodes.peek():
+        print('Updated nodes')
+        for record in updated_nodes:
+            print(record)
+            updateObject(record[0], record[1], record[2])
+    
+    if deleted_edges.peek():
+        print('Deleted edges')
+        for e in deleted_edges:
+            nodes = e[0].nodes
+            removeEdge(nodes[0],nodes[1],e[1])
         
-def createObject(node, labels, keys, edges):
+    if deleted_nodes.peek():
+        print('Deleted nodes')
+        for record in deleted_nodes:
+            deleteObject(record[0], record[1])
+            
+    if detached_nodes.peek():
+        print('Deleted nodes')
+        for record in deleted_nodes:
+            deleteObject(record[0], record[1])
+            
+        to_detach_nodes = session.run("MATCH (n)-[e]->(d) WHERE id(d)= " + record[0].id + " RETURN n, type(e)")
+        
+        for d in to_detach_nodes:
+            removeEdge(d[0],record[0],d[1])
+        
+def createObject(node, labels, keys):
     node_id = node.id
     
     property_values = []
@@ -74,7 +129,72 @@ def createObject(node, labels, keys, edges):
     for k in keys:
         property_values.append(node[k])
     
-    print(node_id, labels, property_values)
+    print(node_id, labels, keys, property_values)
+    
+    objDict = {"id" : str(node_id)}
+    
+    for i in range(len(keys)):
+        objDict[keys[i]] = property_values[i]
+    
+    #print(objDict)
+    if "Person" in labels:
+        p = manager.Person(**objDict)
+        db.actors.insert_one(p.to_dict())
+    else:
+        o = manager.Note(**objDict)
+        db.objects.insert_one(o.to_dict())
+        
+def updateObject(node, labels, keys):
+    node_id = node.id
+    
+    property_values = []
+    
+    for k in keys:
+        property_values.append(node[k])
+    
+    print(node_id, labels, keys, property_values)
+    
+    objDict = {}
+    
+    for i in range(len(keys)):
+        objDict[keys[i]] = property_values[i]
+    
+    if "Person" in labels:
+        updatedNode = db.actors.find_one_and_update({"id": str(node_id)}, {"$set": objDict})
+    else:
+        updatedNode = db.objects.find_one_and_update({"id": str(node_id)}, {"$set": objDict})
+    print(updatedNode)
+    
+def deleteObject(node, labels):
+    node_id = node.id
+    
+    if "Person" in labels:
+        db.actors.remove({"id": str(node_id)})
+    else:
+        db.objects.remove({"id": str(node_id)})
+        
+def createEdge(ingoingNode, outgoingNode, name):
+    id1 = ingoingNode.id
+    id2 = outgoingNode.id
+    
+    print(id1,id2, name)
+    node2 = db.objects.find_one({"id": str(id2)})
+    print(node2)
+    node1 = db.objects.find_one_and_update({"id": str(id1)}, {"$set": {name: node2}})
+    print(node1)
+
+def updateEdge():
+    #TODO ?
+    print('todo updateEdge')
+
+def removeEdge(ingoingNode, outgoingNode, name):
+    id1 = ingoingNode.id
+    id2 = outgoingNode.id
+    
+    print(id1,id2, name)
+    node1 = db.objects.find_one({"id": str(id1)}).__delitem__(name)
+    db.objects.remove({"id": str(id1)})
+    db.actors.insert_one(node1)
 
 checkForUpdates()
 
--- cypher-read.py
+++ cypher-read.py
@@ -28,8 +28,10 @@ def checkQuery (input_query):
     
     if (re.search("CREATE", input_query)):
         input_query = splitQuery(input_query, "CREATE")
-    elif (re.search("UPDATE", input_query)):
-        input_query = splitQuery(input_query, "UPDATE")
+    elif (re.search("SET", input_query)):
+        input_query = splitQuery(input_query, "SET")
+    elif (re.search("DETACH DELETE", input_query)):
+        input_query = splitQuery(input_query, "DETACH")
     elif (re.search("DELETE", input_query)):
         input_query = splitQuery(input_query, "DELETE")
     
@@ -41,10 +43,13 @@ def splitQuery (input_query, query_type):
     regex = "\(([^({]+)(?:({[^(]*)})?\)(?:-\[([^({]+)(?:({[^(]*)})?\]->)?" # TODO: Explanation
     rr_list = []
     carry = ""
+    query_is_split = False
     
     matches = matchQuery(input_query)
+    print('matches', matches)
     
-    create_regex = re.compile("(" + query_type + " \(.*\))(?:\s|$)")
+    #create_regex = re.compile("(" + query_type + " \(.*\))(?:\s|$)")
+    create_regex = re.compile("(" + query_type + " \(?.*\)?)(?:\s|$)")
     
     if not (create_regex.search(input_query)):
         # if correct create syntax not detected, simply send the query so neo4j can send a response
@@ -52,56 +57,69 @@ def splitQuery (input_query, query_type):
         return input_query
     
     split_query = create_regex.split(input_query)
-    #print(split_query)
+    print('split_query', split_query)
     if (len(split_query) == 1):
         create_query = split_query[0]
     elif (len(split_query) == 3):
         create_query = split_query[1]
+        query_is_split = True
+        begin_query = split_query[0]
+        end_query = split_query[2]
     else:
         raise Exception('Unexpected') # TODO: are there other cases that need implementation?
     
     if (query_type == "CREATE"):
         properties = "graph_status:'new'"
-    elif (query_type == "UPDATE"):
+    elif (query_type == "SET"):
         properties = "graph_status:'updated'"
     elif (query_type == "DELETE"):
-        properties = "graph_status:'to delete'"
+        properties = "graph_status:'delete'"
+    elif (query_type == "DETACH"):
+        properties = "graph_status:'detach'"
     else:
         raise Exception('Unexpected query type')
         
     #properties += ", time:'" + str(time()) + "'"
     
-    for r in re.findall(regex, create_query):
-        rr = carry + "("    # if the previous node had an outward pointing edge, it is carried over
-        carry = ""          # reset when carry is completed
-        if r[0].split(':')[0] in matches: # check if there is a match
-            rr += r[0] + r[1]
-            rr += ")" if r[1] == "" else "})"
-        else:
-            if r[1] == "": # check for earlier defined properties
-                rr += r[0] + " {" + properties + "})"
+    if (query_type == "CREATE"):
+        for r in re.findall(regex, create_query):
+            rr = carry + "("    # if the previous node had an outward pointing edge, it is carried over
+            carry = ""          # reset when carry is completed
+            if r[0].split(':')[0] in matches: # check if there is a match
+                rr += r[0] + r[1]
+                rr += ")" if r[1] == "" else "})"
             else:
-                rr += r[0] + r[1] + ", " + properties + "})"
-        if r[2] != "": # check if there is an edge
-            rr += "-["
-            if r[3] == "": # check for earlier defined properties
-                rr += r[2] + " {" + properties + "}]"
+                if r[1] == "": # check for earlier defined properties
+                    rr += r[0] + " {" + properties + "})"
+                else:
+                    rr += r[0] + r[1] + ", " + properties + "})"
+            if r[2] != "": # check if there is an edge
+                rr += "-["
+                if r[3] == "": # check for earlier defined properties
+                    rr += r[2] + " {" + properties + "}]"
+                else:
+                    rr += r[2] + r[3] + ", " + properties + "}]"
+                rr += "->"
+                carry = rr
             else:
-                rr += r[2] + r[3] + ", " + properties + "}]"
-            rr += "->"
-            carry = rr
-        else:
-            rr_list.append(rr)
+                rr_list.append(rr)
+    else:
+        for m in matches:
+            rr_list.append(m + "." + properties)
             
-    print (rr_list)
+    print ("rr_list", rr_list)
     
     if (query_type == "CREATE"):
         return_query = "CREATE " + ', '.join(rr_list)
-    elif (query_type == "UPDATE"):
-        return_query = "UPDATE " + ', '.join(rr_list)
-    elif (query_type == "DELETE"):
-        return_query = "UPDATE " + ', '.join(rr_list) # this UPDATE is intentional
-        
+    elif (query_type == "SET"):
+        return_query = create_query + ', '.join([''] + rr_list)
+    elif (query_type == "DELETE" or query_type == "DETACH"):
+        return_query = "SET " + ', '.join(rr_list) # this SET is intentional, node needs to be preserved and deleted later
+    
+    if (query_is_split):
+        #return_query = re.sub('\s+', ' ', begin_query) + "\n" + return_query + "\n" + re.sub('\s+', ' ', end_query)
+        return_query = begin_query + return_query + "\n" + end_query
+    
     return return_query
             
 def matchQuery(input_query):
@@ -121,4 +139,21 @@ def sendQuery(input_query, query_type=""):
     print(results)
     print()
 
+#%%
 checkQuery(example_query3)
+
+print()
+print()
+
+update_query = """MATCH (n)
+WHERE id(n)=203
+SET n.name = 'Test123'
+RETURN n"""
+
+checkQuery(update_query)
+
+delete_query = """MATCH (n)
+WHERE id(n)=203
+DETACH DELETE n"""
+
+checkQuery(delete_query)
